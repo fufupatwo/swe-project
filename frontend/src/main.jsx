e4857c2615802e7f68079bad1abdbe0d2632b9c1
fufupatwo <aaronadelgadoo@gmail.com>
2024-04-19 15:54:29 -0500

    Set up Routes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,1 +1,28 @@
-frontend/src/App.jsx
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider} from 'react-router-dom' 
+import './index.css'
+import LandingPage from './pages/LandingPage'
+import LoginPage from './pages/LoginPage'
+import CreateAccount from './pages/CreateAccount'
+
+const router = createBrowserRouter([
+  {
+  path: '/',
+  element: <LandingPage />
+  },
+  {
+    path: '/Login',
+    element: <LoginPage />
+  },
+  {
+  path:'CreateAccount',
+  element: <CreateAccount />
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
