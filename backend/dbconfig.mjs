85f57c24c4c9d235edab01b59d0e9a000d63f991
fufupatwo <aarond99@hotmail.com>
2024-04-19 16:44:18 -0500

    dbconfig file

---
+++ backend/dbconfig.mjs
@@ -0,0 +1,23 @@
+import mysql from "mysql";
+import dotenv from "dotenv";
+import express from "express";
+
+dotenv.config();
+const DB_HOST = process.env.DBHOST
+const DB_USER = process.env.DBUSER
+const DB_PASSWORD = process.env.DBPASSWORD
+const DB_DATABASE = process.env.DBNAME
+const DB_PORT = process.env.DBPORT
+
+
+const app = express();
+
+const db = mysql.createPool({
+    connectionLimit: 100,
+    host: DB_HOST,
+    user: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_DATABASE,
+    port: DB_PORT
+});
+export default db;
