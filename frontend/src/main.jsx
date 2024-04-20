4e22d11d2b1b53700fa33c342a0eb6f532e581c6
Anh Vuong <anhvuong@Anhs-MBP.attlocal.net>
2024-04-20 15:00:17 -0500

    route-debug

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,28 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider} from 'react-router-dom' 
-import './index.css'
-import LandingPage from './pages/LandingPage'
-import LoginPage from './pages/LoginPage'
-import CreateAccount from './pages/CreateAccount'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'; 
+import './index.css';
+import LandingPage from './pages/LandingPage';
+import LoginPage from './pages/LoginPage';
+import CreateAccount from './pages/CreateAccount';
 
 const router = createBrowserRouter([
   {
-  path: '/',
-  element: <LandingPage />
+    path: '/',
+    element: <LandingPage />
   },
   {
     path: '/Login',
     element: <LoginPage />
   },
   {
-  path:'CreateAccount',
-  element: <CreateAccount />
+    path:'CreateAccount',
+    element: <CreateAccount />
   }
-])
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
--- frontend/src/pages/CreateAccount.jsx
+++ frontend/src/pages/CreateAccount.jsx
@@ -1,99 +1,75 @@
-/*
-  This example requires some changes to your config:
-  
-  ```
-  // tailwind.config.js
-  module.exports = {
-    // ...
-    plugins: [
-      // ...
-      require('@tailwindcss/forms'),
-    ],
-  }
-  ```
-*/
-export default function CreateAccount() {
-  return (
-    <>
-      {/*
-        This example requires updating your template:
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
-        ```
-        <html class="h-full bg-white">
-        <body class="h-full">
-        ```
-      */}
-      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <img
-            className="mx-auto h-10 w-auto"
-            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-            alt="Your Company"
-          />
-          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-            Sign in to your account
+export default function CreateAccount() {
+  const navigate = useNavigate();
+
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+    passwordConfirm: ''
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post('/register', formData);
+      console.log(res.data);
+      navigate('/LandingPage');
+    } catch (error) {
+      console.error("An error occurred:", error);
+    }
+  };
+
+  return (
+    <div className="flex min-h-full items-center justify-center px-6 py-12 lg:px-8">
+      <div className="w-96 p-6 items-center justify-center shadow-lg bg-orange-500 rounded-md">
+        <div className="sm:w-full items-center justify-center">
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 text-gray-900">
+            Create Account
           </h2>
         </div>
-
-        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <form className="space-y-6" action="#" method="POST">
-            <div>
-              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-                Email address
-              </label>
-              <div className="mt-2">
-                <input
-                  id="email"
-                  name="email"
-                  type="email"
-                  autoComplete="email"
-                  required
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
-
-            <div>
-              <div className="flex items-center justify-between">
-                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
-                  Password
-                </label>
-                <div className="text-sm">
-                  <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
-                    Forgot password?
-                  </a>
-                </div>
-              </div>
-              <div className="mt-2">
-                <input
-                  id="password"
-                  name="password"
-                  type="password"
-                  autoComplete="current-password"
-                  required
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
-
-            <div>
-              <button
-                type="submit"
-                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
-                Sign in
-              </button>
-            </div>
-          </form>
-
-          <p className="mt-10 text-center text-sm text-gray-500">
-            Not a member?{' '}
-            <a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
-              Start a 14 day free trial
-            </a>
-          </p>
-        </div>
+        <form onSubmit={handleSubmit}>
+          <div className="mt-6">
+            <label htmlFor="firstName" className="block text-sm font-medium leading-6 text-gray-50">First Name</label>
+            <input onChange={handleChange} id="firstName" name="firstName" type="text" autoComplete="given-name" required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+          </div>
+          <div className="mt-6">
+            <label htmlFor="lastName" className="block text-sm font-medium leading-6 text-gray-50">Last Name</label>
+            <input onChange={handleChange} id="lastName" name="lastName" type="text" autoComplete="family-name" required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+          </div>
+          <div className="mt-6">
+            <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-50">Email</label>
+            <input onChange={handleChange} id="email" name="email" type="email" autoComplete="email" required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+          </div>
+          <div className="mt-6">
+            <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-50">Password</label>
+            <input onChange={handleChange} id="password" name="password" type="password" autoComplete="new-password" required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+          </div>
+          <div className="mt-6">
+            <label htmlFor="passwordConfirm" className="block text-sm font-medium leading-6 text-gray-50">Confirm Password</label>
+            <input onChange={handleChange} id="passwordConfirm" name="passwordConfirm" type="password" autoComplete="new-password" required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+          </div>
+          <div className="mt-6">
+            <button type="submit" className="w-full rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Create Account</button>
+          </div>
+        </form>
+        <p className="mt-2 text-center text-sm text-gray-500">
+          Already have an account?{' '}
+          <a href="/Login" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Login</a>
+        </p>
       </div>
-    </>
-  )
+    </div>
+  );
 }
