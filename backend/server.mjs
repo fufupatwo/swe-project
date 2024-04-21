c1443225bf67b8010c2288fdda8f3503a4810520
Anh Vuong <anhvuong@Anhs-MBP.attlocal.net>
2024-04-20 23:59:34 -0500

    create posting route

--- backend/server.mjs
+++ backend/server.mjs
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
-import {createUserRoute, loginRoute, adminLoginRoute} from "./routes.mjs";
-import cors from "cors"
+import {createUserRoute, loginRoute, adminLoginRoute, postCreationRoute} from "./routes.mjs";
+import cors from "cors";
 
 
 
@@ -15,6 +15,7 @@ app.use(cors());
 app.post("/register", createUserRoute);
 app.post("/login", loginRoute);
 app.post("/admin", adminLoginRoute);
+app.post("/post_creation",postCreationRoute);
 
 
 const PORT = process.env.PORT || 4000; // Default port 5173
