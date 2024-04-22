ff6d254363dc6533974097f4c6ef4b983d266f6c
fufupatwo <aarond99@hotmail.com>
2024-04-21 21:44:18 -0500

    added Forgot password link to /forgot_password_page

--- frontend/src/pages/CreateAccount.jsx
+++ frontend/src/pages/CreateAccount.jsx
@@ -49,28 +49,44 @@ export default function CreateAccount() {
         <form onSubmit={handleSubmit}>
           <div className="mt-6">
             <label htmlFor="firstName" className="block text-sm font-medium leading-6 text-gray-50">First Name</label>
-            <input onChange={handleChange} id="firstName" name="firstName" type="text" autoComplete="given-name" required className="block w-full rounded-md border-0 py-1.5 text-gray-300 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+            <input onChange={handleChange} id="firstName" name="firstName" type="text" autoComplete="given-name"
+                   required
+                   className="block w-full rounded-md border-0 py-1.5 text-gray-300 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
           </div>
           <div className="mt-6">
             <label htmlFor="lastName" className="block text-sm font-medium leading-6 text-gray-50">Last Name</label>
-            <input onChange={handleChange} id="lastName" name="lastName" type="text" autoComplete="family-name" required className="block w-full rounded-md border-0 py-1.5 text-gray-300 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+            <input onChange={handleChange} id="lastName" name="lastName" type="text" autoComplete="family-name" required
+                   className="block w-full rounded-md border-0 py-1.5 text-gray-300 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
           </div>
           <div className="mt-6">
             <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-50">Email</label>
-            <input onChange={handleChange} id="email" name="email" type="email" autoComplete="email" required className="block w-full rounded-md border-0 py-1.5 text-gray-300 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+            <input onChange={handleChange} id="email" name="email" type="email" autoComplete="email" required
+                   className="block w-full rounded-md border-0 py-1.5 text-gray-300 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
           </div>
           <div className="mt-6">
             <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-50">Password</label>
-            <input onChange={handleChange} id="password" name="password" type="password" autoComplete="new-password" required className="block w-full rounded-md border-0 py-1.5 text-gray-300 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+            <input onChange={handleChange} id="password" name="password" type="password" autoComplete="new-password"
+                   required
+                   className="block w-full rounded-md border-0 py-1.5 text-gray-300 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
           </div>
           <div className="mt-6">
-            <label htmlFor="passwordConfirm" className="block text-sm font-medium leading-6 text-gray-50">Confirm Password</label>
-            <input onChange={handleChange} id="passwordConfirm" name="passwordConfirm" type="password" autoComplete="new-password" required className="block w-full rounded-md border-0 py-1.5 text-gray-300 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+            <label htmlFor="passwordConfirm" className="block text-sm font-medium leading-6 text-gray-50">Confirm
+              Password</label>
+            <input onChange={handleChange} id="passwordConfirm" name="passwordConfirm" type="password"
+                   autoComplete="new-password" required
+                   className="block w-full rounded-md border-0 py-1.5 text-gray-300 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
           </div>
           <div className="mt-6">
-            <button type="submit" className="w-full rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Create Account</button>
+            <button type="submit"
+                    className="w-full rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Create
+              Account
+            </button>
           </div>
         </form>
+        <p className="mt-2 text-center text-sm text-gray-500">
+          {' '}
+          <a href="/forgot_password_page" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Forgot Password?</a>
+        </p>
         <p className="mt-2 text-center text-sm text-gray-500">
           Already have an account?{' '}
           <a href="/" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Login</a>
