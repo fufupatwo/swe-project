f67becf11287047edc869bfa62ffa6eaffcf3627
fufupatwo <aarond99@hotmail.com>
2024-04-28 11:44:58 -0500

    fixed login page, with links, text

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -53,7 +53,7 @@ export default function LoginPage() {
               <label htmlFor="password" className="block text-sm font-bold leading-6 text-gray-900">Password</label>
               <input onChange={handleChange} id="password" name="password" type="password"
                      autoComplete="current-password" value={formData.password} required
-                     placeholder=" Enter your password"
+                     placeholder="Enter your password"
                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
             </div>
 
