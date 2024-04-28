88507307648838c319bdcb841a4a2348c1e8be7c
Anh Vuong <annalisaatv@gmail.com>
2024-04-27 19:12:12 -0500

    adjust recent post color

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -105,7 +105,7 @@ const HomePage = () => {
   return (
       <div>
         <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-          <h2 className="text-2xl font-bold tracking-tight text-gray-900">Recent Posts</h2>
+          <h2 className="text-2xl font-bold tracking-tight text-gray-300">Recent Posts</h2>
 
           <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
             {posts.map((post, index) => (
