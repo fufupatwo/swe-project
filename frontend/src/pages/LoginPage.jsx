23d18a36831d28a4a8d452a3455e345ed1d18cd0
fufupatwo <aarond99@hotmail.com>
2024-04-28 11:08:47 -0500

    Added forgot password to sign in page

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -42,23 +42,38 @@ export default function LoginPage() {
           <form onSubmit={handleSubmit} className="mt-6 w-full">
             <div className="mb-6 w-full">
               <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">Email</label>
-              <input onChange={handleChange} id="useremail" name="useremail" type="email" autoComplete="email" value={formData.useremail} required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+              <input onChange={handleChange} id="useremail" name="useremail" type="email" autoComplete="email"
+                     value={formData.useremail} required
+                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
             </div>
 
             <div className="mb-6 w-full">
               <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">Password</label>
-              <input onChange={handleChange} id="password" name="password" type="password" autoComplete="current-password" value={formData.password} required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+              <input onChange={handleChange} id="password" name="password" type="password"
+                     autoComplete="current-password" value={formData.password} required
+                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
             </div>
 
             <div className="w-full">
-              <button type="submit" className="w-full rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
+              <button type="submit"
+                      className="w-full rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign
+                in
+              </button>
             </div>
           </form>
 
           <p className="mt-2 text-center text-sm text-gray-500">
             Not a member?{' '}
-            <a href="/CreateAccount" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Create Account</a>
+            <a href="/CreateAccount" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Create
+              Account</a>
           </p>
+
+          <p className="mt-2 text-center text-sm text-gray-500">
+            {' '}
+            <a href="/ForgotPasswordPage" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Forgot
+              Password?</a>
+          </p>
+
         </div>
       </div>
   );
